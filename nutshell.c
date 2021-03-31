4732b22519a809dad918084d95bd8e2876cbf4eb
triple3567 <ericvho888@gmail.com>
2021-03-31 22:11:54 +0000

    alias, cd, bye, and printenv functionality

--- nutshell.c
+++ nutshell.c
@@ -1,40 +1,51 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-#include "nutshell.h"
-//#include "nutshscanner.h"
-#include "nutshparser.tab.h"
+#include <string.h>
+#include "global.h"
+#include <unistd.h>
+#include <limits.h>
 
-void printPrompt()
-{
-    char dir[256];
-    getcwd(dir,256);
-    printf("user@computer:%s>> ", dir);
-}
-
-int getCommand()
-{
-    printPrompt();
-    yyparse();
-
-    enum cmdResponse resp;
-    printf("[ Command: ] %s\n", cmd);
-    if(strcmp("bye", cmd) == 0)
-        resp = BYE;
-    else
-        resp = OK;
-    return resp;
-}
+char *getcwd(char *buf, size_t size);
 
 int main()
 {
-	printf("Welcome to the Nutshell\n");
-    while(1){
-        int CMD;
-        switch(CMD = getCommand())
-        {
-        case BYE:       exit(0);
-        case OK:        printf("OK\n");
-        }
+    aliasIndex = 0;
+    varIndex = 0;
+    char cwd[PATH_MAX];
+    getcwd(cwd, sizeof(cwd));
+
+    strcpy(varTable.var[varIndex], "PWD");
+    strcpy(varTable.word[varIndex], cwd);
+    varIndex++;
+    strcpy(varTable.var[varIndex], "HOME");
+    strcpy(varTable.word[varIndex], cwd);
+    varIndex++;
+    strcpy(varTable.var[varIndex], "PROMPT");
+    strcpy(varTable.word[varIndex], "nutshell");
+    varIndex++;
+    strcpy(varTable.var[varIndex], "PATH");
+    strcpy(varTable.word[varIndex], ".:/bin");
+    varIndex++;
+
+    strcpy(aliasTable.name[aliasIndex], ".");
+    strcpy(aliasTable.word[aliasIndex], cwd);
+    aliasIndex++;
+
+    char *pointer = strrchr(cwd, '/');
+    while(*pointer != '\0') {
+        *pointer ='\0';
+        pointer++;
     }
+    strcpy(aliasTable.name[aliasIndex], "..");
+    strcpy(aliasTable.word[aliasIndex], cwd);
+    aliasIndex++;
+    system("clear");
+	printf("Welcome to the Nutshell\n");
+    
+    while(1){
+        printf("[%s]>> ", varTable.word[0]);
+        yyparse();
+    }
+
+    return 0;
 }
